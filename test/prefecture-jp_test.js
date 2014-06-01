fc729435cc5d21ae8a293ad8034fe99a155c9c1e
sanemat <o.gata.ken@gmail.com>
2014-06-01 20:30:06 +0900

    Add fake implement

--- test/prefecture-jp_test.js
+++ test/prefecture-jp_test.js
@@ -3,10 +3,17 @@
 var prefectureJp = require('../lib/prefecture-jp.js');
 var assert = require('power-assert');
 
-describe('prefectureJp', function () {
+describe('prefectureJp JIS X 0401', function () {
+  var prefs;
 
-  it('should be awesome', function () {
-    assert.equal(prefectureJp.awesome(), 'awesome');
+  before(function(done){
+    prefs = prefectureJp({schema: 'jisx0401'});
+    done();
   });
-
+  it('should return pref', function(done){
+    prefs.search({ pref: '京都' }, function(target){
+      assert.equal(target, {code:"26",pref:"京都府"});
+      done();
+    })
+  });
 });
