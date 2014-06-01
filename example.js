5a265ad2e5f1af6c1148a86c3119f8887fdf131b
sanemat <o.gata.ken@gmail.com>
2014-06-01 19:51:54 +0900

    Add example

---
+++ example.js
@@ -0,0 +1,14 @@
+var prefectureJp = require('prefectureJp');
+var prefs = prefectureJp({schema: 'jisx0401'});
+
+var pref1 = prefs.filter(function(pref){
+  return pref.code == '26';
+});
+pref1.code;// => '26'
+pref1.pref;// => '京都府'
+
+var pref2 = prefs.filter(function(pref){
+  return pref.pref == '京都府';
+});
+pref2.code;// => '26'
+pref2.pref;// => '京都府'
