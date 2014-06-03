1ef743a32d232828deab257b763f1608d43e3843
sanemat <o.gata.ken@gmail.com>
2014-06-03 16:20:28 +0900

    Change interface

--- example/simple.js
+++ example/simple.js
@@ -25,7 +25,8 @@
 var prefectureJp = require('../lib/prefecture-jp.js');
 var prefs = prefectureJp({schema: 'jisx0401'});
 
-prefs.search({pref: '京都'}, function(callback){
-  callback.code;// => '26'
-  callback.pref;// => '京都府'
+prefs.search({pref: '愛知'}, function(callback){
+  var pref = callback.shift();
+  pref.code;// => '23'
+  pref.pref;// => '愛知県'
 });
